9e282f4d907fd09c81c02c502421415881217512
takumi_Mac <>
2024-02-11 15:02:57 +0900

    showページ

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  root to: 'articles#index'
   
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root to: 'articles#index'
+
+  resources :articles
 end
